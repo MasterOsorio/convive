5d3a602349e48debda9d7de449a34c11d88a535e
Juan Osorio <kissthbw@gmail.com>
2021-01-22 11:03:47 -0600

    Add: Save and list

--- src/main/java/com/bit/management/catalogs/area/CatalogoAreaController.java
+++ src/main/java/com/bit/management/catalogs/area/CatalogoAreaController.java
@@ -1,5 +1,10 @@
 package com.bit.management.catalogs.area;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +14,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 @Controller
 @RequestMapping(value = "catalogs/catalog-area")
@@ -29,9 +36,43 @@ public class CatalogoAreaController {
 	}
 
 	@PostMapping(value = "agregar")
-	public String guardarCatalogoArea(Model model, @ModelAttribute CatalogoAreaView item, BindingResult errors) {
+	public String guardarCatalogoArea(Model model, 
+			@ModelAttribute CatalogoAreaView item, BindingResult errors,
+			RedirectAttributes redirectAttributes) {
 
 		catalogoAreaService.guardar(item);
-		return "redirect:/webapp/catalogs/template-catalog-area";
+		boolean showErrores = false;
+		
+		if( showErrores ) {
+			model.addAttribute("error", true);
+			model.addAttribute("message", "Servicio no disponible, intentar en un momento");
+			model.addAttribute("viewdataform", item);
+			return "catalogs/template-catalog-area";
+		}
+		else {
+			redirectAttributes.addFlashAttribute("action", "success");
+			redirectAttributes.addFlashAttribute("message", "El registro ha sido guardado correctamente");
+			
+			return "redirect:list" ;
+		}
+	}
+	
+	@GetMapping(value = "list")
+	public String getCatalogoAreaList(Model model, HttpServletRequest request) {
+
+		Map<String, ?> inputFlashMap = RequestContextUtils.getInputFlashMap(request);
+		if (inputFlashMap != null) {
+			String action = (String)inputFlashMap.get("action");
+			String message = (String)inputFlashMap.get("message");
+			
+			model.addAttribute("action", action);
+			model.addAttribute("message", message);
+		}
+
+		List<CatalogoAreaView> list = catalogoAreaService.list( null );
+		model.addAttribute("list", list);
+		
+		
+		return "catalogs/catalog-area-list";
 	}
 }
--- src/main/java/com/bit/management/catalogs/area/CatalogoAreaServiceImpl.java
+++ src/main/java/com/bit/management/catalogs/area/CatalogoAreaServiceImpl.java
@@ -1,21 +1,55 @@
 package com.bit.management.catalogs.area;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CatalogoAreaServiceImpl implements CatalogoAreaService {
 	
 	private static Logger log = LoggerFactory.getLogger(CatalogoAreaServiceImpl.class);
 	
+	@Autowired
+	private CatalogoAreaDAO catalogoAreaDAO; 
+	
 	@Override
+	@Transactional
 	public CatalogoAreaView guardar(CatalogoAreaView item) {
 		
 		log.info("Guardando en base de datos");
-		CatalogoAreaView a = new CatalogoAreaView();
+
 		
-		return a;
+		//1. Convertir de CatalogoAreaView a CatalogoArea
+		CatalogoArea entity = new CatalogoArea();
+		entity.setArea( item.getArea() );
+		entity = catalogoAreaDAO.save(entity);
+		
+		item.setId( entity.getId() );
+		
+		return item;
+	}
+
+	@Override
+	@Transactional
+	public List<CatalogoAreaView> list(CatalogoAreaView item) {
+		List<CatalogoArea> list = catalogoAreaDAO.list();
+		List<CatalogoAreaView> listViews = new ArrayList<>();
+		
+		for( CatalogoArea e : list ) {
+			CatalogoAreaView v = new CatalogoAreaView();
+			v.setId( e.getId() );
+			v.setArea( e.getArea() );
+			listViews.add(v);
+		}
+		
+		
+		return listViews;
+		
 	}
 
 }
